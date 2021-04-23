137b202a3e7bcd41bebd06828af7a596c8c71ae7
YuqiaoBai <byq93715@gmail.com>
2021-04-23 16:41:43 +0200

    run

--- CNN/model.py
+++ CNN/model.py
@@ -1,22 +1,21 @@
 import torch
 import torch.nn as nn
 
-
 class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layer1 = nn.Conv2d(in_channels=21, out_channels=64, kernel_size=2, stride=2, bias=False)
-        self.layer2 = nn.Conv2d(64, 128, kernel_size=2, stride=2,  bias=False)
-        self.layer3 = nn.Conv2d(128, 256, kernel_size=2, stride=2,  bias=False)
-        self.layer4 = nn.Conv2d(256, 512, kernel_size=2, stride=2,  bias=False)
+        self.layer1 = nn.Conv2d(in_channels=21, out_channels=64, kernel_size=2, stride=2)
+        self.layer2 = nn.Conv2d(64, 128, kernel_size=2, stride=2)
+        self.layer3 = nn.Conv2d(128, 256, kernel_size=2, stride=2)
+        self.layer4 = nn.Conv2d(256, 512, kernel_size=2, stride=2)
         self.decode4 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
         self.decode3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
         self.decode2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
         self.decode1 = nn.ConvTranspose2d(64, 20, kernel_size=2, stride=2)
 
     def forward(self, input):
-        print('in forward:', input.dtype) #(2,21,256,256)
+        # print('in forward:', input.dtype) #(2,21,256,256)
         e1 = self.layer1(input) #(2,64,130,130)
         e2 = self.layer2(e1) #(2,128,67,67)
         e3 = self.layer3(e2) #(2,256,36,36)
@@ -26,9 +25,25 @@ class ConvNet(nn.Module):
         d2 = self.decode3(d3)
         d1 = self.decode2(d2)
         out = self.decode1(d1)
-        torch.where(out>0,1)
-        torch.where(out<0,0)
-        print('greater than 0:', torch.sum(out>0))
-        print('smaller than 0:', torch.sum(out < 0))
+
+        x = torch.ones(out.shape).cuda()
+        y = torch.zeros(out.shape).cuda()
+        out = torch.where(out > 0, x, y)
+        # print('greater than 0:', torch.sum(out > 0))
+        # print('smaller than 0:', torch.sum(out < 0))
         return out
 
+
+class PointsLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        return
+
+    def forward(self, added_points, original_point):
+        added_points = torch.sum(added_points, axis=1)
+        convert_points = (added_points == 0)
+        ego_points = original_point[:, 1, :, :]
+        a = torch.sum(convert_points.cuda() != ego_points.cuda())
+        b = torch.sum(ego_points)
+        loss = a/b
+        return loss
--- CNN/train_cnn.py
+++ CNN/train_cnn.py
@@ -1,60 +1,47 @@
-from model import ConvNet
+from model import ConvNet, PointsLoss
 import torch
 from dataloader import coopDataset
 from torch.utils.data import DataLoader
 import torch.optim as optim
-from ops.roiaware_pool3d import roiaware_pool3d_cuda
+from tqdm import tqdm
 
-def points_in_boxes_gpu(points, boxes):
-    """
-    :param points: (B, M, 3)
-    :param boxes: (B, T, 7), num_valid_boxes <= T
-    :return box_idxs_of_pts: (B, M), default background = -1
-    """
-    assert boxes.shape[0] == points.shape[0]
-    assert boxes.shape[2] == 7 and points.shape[2] == 3
-    batch_size, num_points, _ = points.shape
 
-    box_idxs_of_pts = points.new_zeros((batch_size, num_points), dtype=torch.int).fill_(-1)
-    roiaware_pool3d_cuda.points_in_boxes_gpu(boxes.contiguous(), points.contiguous(), box_idxs_of_pts)
-
-    return box_idxs_of_pts
-
-def loss_function(output, gt):
-
-    return
-
-def data_mask():
-    pass
+def data_mask(mask, points):
+    points = points[:, 1:, :, :]
+    fused_points = mask * points
+    return fused_points
 
 
 epoches = 2
-batch_size = 2
+batch_size = 20
 learning_rate = 0.001
 
 # Load data
 train_data = coopDataset()
-train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=6)
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print(device)
 # Model
 Coop = ConvNet()
-Coop = Coop.float().to(device)
+Coop = Coop.float().cuda()
 # Define optimizer and loss function
-optimizer = optim.Adam(Coop.parameters(), lr=learning_rate)
+optimizer = optim.SGD(Coop.parameters(), lr=learning_rate)
 
 
 # Train
 for epoch in range(epoches):
     print('epoch:{}'.format(epoch))
-    for step, cnn_input in enumerate(train_loader):
-        output = Coop(cnn_input["points"].float().to(device))
-        loss = loss_function(output, cnn_input["gt_boxes"])
+    for step, cnn_input in tqdm(enumerate(train_loader)):
+        output = Coop(cnn_input["points"].float().cuda())
+        points_fused = data_mask(output, cnn_input["points"].float().cuda())
+        loss_function = PointsLoss()
+        loss = loss_function(points_fused, cnn_input["points"])
+        loss.requires_grad = True
         optimizer.zero_grad()
+        loss.backward()
         optimizer.step()
 
         if step % 10 == 0:
-            test_output = Coop(cnn_input)
-            gt = 'to do'
             print('Epoch:', epoch, '|loss:', loss)
 
 
